92128dc502eb630c7accba15f38d2db3659d5283
冀刚 <jiweigang2008@126.com>
2021-11-18 15:18:04 +0000

    index

--- src/window.js
+++ src/window.js
@@ -1,26 +1,80 @@
+var Content = {
+    props:{
+        com:Object
+    },
+    mounted:function(){
+        this.$emit("created",this.$refs.content);
+    },
+    render: function (createElement)  {
+        //应该判断是否为字符字符串
+        return createElement(this.com,{
+            ref:"content"
+        });
+    }
+}
+
+
 var Window = {
     props:{
         win:Object
     },
+    computed:{
+        title:function(){
+            return this.win.title?this.win.title:"";
+        },
+        size:function(){
+            var width  = this.win.width?this.win.width:500;
+            var height = this.win.height?this.win.height:300;
+            return {width:width,"max-width":width,height:height,"max-height":height};
+        },
+        cHeight:function(){
+            var     height = this.win.height?this.win.height:300;
+            height =  height - 60- 70;
+            return height;
+        }
+    },
+    methods:{
+        ok:function(){
+            //调用组件的函数
+            if(this.content["ok"]){
+               this.content["ok"]();
+            }
+            this.$emit('ok');
+        },
+        cancel:function(){
+            //调用组件的函数
+            if(this.content["cancel"]){
+                this.content["cancel"]();
+            }
+            this.$emit('cancel');
+        },
+        //内容初始化完成
+        created:function(content){
+            this.content = content;
+        }
+    },
+    components: {
+        "content-com":Content
+    },
     template:
-        '<div class="modal-dialog">\
+        '<div class="modal-dialog" :style="size" >\
             <div class="modal-content">\
                 <div class="modal-header">\
-                    <h5 class="modal-title">Modal title</h5>\
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
-                    <span aria-hidden="true">&times;</span>\
+                    <h5 class="modal-title">{{title}}</h5>\
+                    <button @click="cancel" type="button" class="close" data-dismiss="modal" aria-label="Close">\
+                        <span aria-hidden="true">&times;</span>\
                     </button>\
                 </div>\
-                <div class="modal-body">\
-                    <p>Modal body text goes here.</p>\
+                <div class="modal-body" :style="{height:cHeight}" >\
+                   <content-com @created="created" :com="win.com"></content-com>\
                 </div>\
                 <div class="modal-footer">\
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
-                    <button type="button" class="btn btn-primary">Save changes</button>\
+                    <button @click="cancel" type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>\
+                    <button @click="ok" type="button" class="btn btn-primary">确定</button>\
                 </div>\
             </div>\
         </div>'
-}
+    }
 
 var WindowContainer = {
     data: function () {
@@ -29,21 +83,44 @@ var WindowContainer = {
         }
     },
     methods:{
-        openWindow: function(window){
-            windows.push(window);
+        open: function(win){
+            this.windows = this.windows.concat(win);
+        },
+        ok:function(win){
+            this.delete(win);
+        },
+        cancel:function(win){
+            this.delete(win);
+        },
+        //删除window
+        delete:function(win){
+           for(var i =0; i< this.windows.length; i++){
+             if(this.windows[i] == win){
+                this.windows.splice(i, 1);
+                this.windows = this.windows.concat([]);
+                break;
+             }
+           } 
         }
     },
+    components:{
+        "window":Window
+    },
     template:
-            '<template v-for="win in windows">\
-                <window :win="win"></window>\
-             <template>'
+            '<div>\
+                <template v-for="win in windows">\
+                    <window @ok="ok(win)" @cancel="cancel(win)" :win="win"></window>\
+                <template>\
+             </div>'
 }
 
 export var WindowPlugin = {
     install:function(Vue, options){
         var WindowContainerComp = Vue.extend(WindowContainer);
-        var WindowContainerComp = new WindowContainer().$mount('#body');
-        Vue.WindowContainerComp = WindowContainerComp;
+        var win = new WindowContainerComp().$mount();
+        document.body.appendChild(win.$el);
+
+        Vue.WindowContainerComp = win;
 
         Vue.prototype.$openWindow = function (window) {
             Vue.WindowContainerComp.open(window);
