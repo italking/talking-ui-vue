f16d3b2dc44eba137b4abb1af01ba084f50f2354
冀刚 <jiweigang2010@126.com>
2021-12-29 16:57:27 +0000

    增加 confirm 功能

--- src/alertify.js
+++ src/alertify.js
@@ -1,3 +1,43 @@
+var Confirm = {
+    props: {
+        confirm:{}
+    },
+    methods:{
+        close:function(){
+            if(this.confirm.cancel){
+                this.confirm.cancel();
+            }
+            this.$emit('close');
+        },
+        ok:function(){
+            if(this.confirm.ok){
+                this.confirm.ok();
+            }
+            this.$emit('close');
+        }
+    },
+    template: 
+     '<div style="display:block;background-color:rgba(157, 157, 157,0.5);position:fixed;left:0;top:0;height:100%;width:100%">\
+        <div class="modal-dialog">\
+            <div class="modal-content">\
+            <div v-if="confirm.showHeader" class="modal-header">\
+                <h5 class="modal-title">{{confirm.title}}</h5>\
+                <button type="button" class="close" @click="close" >\
+                    <span aria-hidden="true">&times;</span>\
+                </button>\
+            </div>\
+            <div class="modal-body">\
+                {{confirm.message}}\
+            </div>\
+            <div class="modal-footer">\
+                <button type="button" class="btn btn-secondary" @click="close" >取消</button>\
+                <button type="button" class="btn btn-primary" @click="ok" >确定</button>\
+            </div>\
+            </div>\
+       </div>\
+     </div>'
+}
+
 var Alertify = {
     props: {
           message:null
@@ -38,11 +78,14 @@ var Alertify = {
 var AlertifyContainer = {
     data: function () {
         return {
-          messages:[]
+          messages:[],
+          currentConfirm:null,
+          confirms:[]
         }
     },
     components:{
-        alertify:Alertify
+        alertify:Alertify,
+        confirm:Confirm
     },
     methods:{
         //如果设置了时间可以自动关闭
@@ -51,6 +94,20 @@ var AlertifyContainer = {
                 this.setTimeout(message);
             }
             this.messages = this.messages.concat(message);
+        },
+        confirm:function(confirm){
+            this.confirms.push(confirm);
+            this.nextConfirm();
+        },
+        //处理下一个 confirm 
+        nextConfirm:function(){
+            if(this.currentConfirm === null && this.confirms.length > 0){
+                this.currentConfirm = this.confirms.pop();
+            }
+        },
+        closeConfirm:function(){
+            this.currentConfirm = null;
+            this.nextConfirm();
         },
         close(message){
             this.delete(message);
@@ -74,17 +131,32 @@ var AlertifyContainer = {
     },
     //zindex loadding > message > model
     template:
-        '<div style="width:600px;z-index: 5555; position:fixed !important; right: 0px; top: 0px;">\
-            <template v-for="message in messages">\
-                <alertify @close="close(message)" :message="message"></alertify>\
-            <template>\
-         <div>'
+        '<div>\
+            <confirm v-if="currentConfirm" @close="closeConfirm()" :confirm="currentConfirm"></confirm>\
+            <div style="width:600px;position:fixed !important; right: 4px; top: 10px">\
+                <template v-for="message in messages">\
+                    <alertify @close="close(message)" :message="message"></alertify>\
+                <template>\
+            </div>\
+         </div>'
 }
 /**
+ * Message
+ * 
  * level: 等级 3 成功 2 警告 1 危险
  * message：消息内容
  * time：自动关闭等待时间，毫秒
  * closeable：是否支持手动关闭
+ * 
+ * -----
+ * Confirm
+ * showHeader:显示头
+ * title：标题
+ * message：消息内容
+ * ok：确认函数
+ * cancle：取消函数
+ * 
+ * 
  */
 export var AlertifyPlugin = {
     install:function(Vue, options){
@@ -96,6 +168,15 @@ export var AlertifyPlugin = {
 
         Vue.prototype.$notify = function (message) {
             Vue.AlertifyContainerComp.notify(message);
+        }
+
+        Vue.prototype.$alertify = {
+            message:function(message){
+                Vue.AlertifyContainerComp.notify(message);
+            },
+            confirm:function(confirm){
+                Vue.AlertifyContainerComp.confirm(confirm);
+            }
         }
     }
 }
